ed438a7e12e7f42a821f9cd4dcd19aedb38fb428
云 陆 <luyun215@hotmail.com>
2018-11-20 19:56:49 +0800

    1

---
+++ ie自动填写/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ie自动填写
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 144);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(379, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(96, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 169);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ ie自动填写/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ie自动填写
+{
+    public partial class Form1 : Form
+    {
+        private string[] s;
+        private string webaddr;
+        private string webuserid;
+        private string webpassword;
+        private string userid;
+        private string password;
+        private string webclick;
+        public Form1(string[] p)
+        {
+            InitializeComponent();
+            s = p;                        
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text=(s[0]);
+           webaddr=s[0].Substring(0, s[0].IndexOf("="));
+            s[0] = s[0].Remove(0, s[0].IndexOf("=")+1);
+            webuserid= s[0].Substring(0, s[0].IndexOf("="));
+            s[0] = s[0].Remove(0, s[0].IndexOf("=") + 1);
+            userid = s[0].Substring(0, s[0].IndexOf("="));
+            s[0] = s[0].Remove(0, s[0].IndexOf("=") + 1);
+            webpassword = s[0].Substring(0, s[0].IndexOf("="));
+            s[0] = s[0].Remove(0, s[0].IndexOf("=") + 1);
+            password = s[0].Substring(0, s[0].IndexOf("="));
+            s[0] = s[0].Remove(0, s[0].IndexOf("=") + 1);
+            webclick = s[0];
+            SHDocVw.InternetExplorer ie = new SHDocVw.InternetExplorer();
+            ie.DocumentComplete += ie_DocomentComplete;
+            ie.Navigate(webaddr);
+            ie.Visible = true;
+            compWait();
+            mshtml.HTMLDocument doc = ie.Document;
+            doc.getElementById(webuserid).innerText = userid;
+            doc.getElementById(webpassword).innerText = password;
+            System.Threading.Thread.Sleep(1000);
+            doc.getElementById(webclick).click();
+            System.Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SHDocVw.InternetExplorer ie = new SHDocVw.InternetExplorer();
+            ie.DocumentComplete += ie_DocomentComplete;
+            ie.Navigate("http://192.168.32.121/");
+            ie.Visible = true;
+            compWait();
+            mshtml.HTMLDocument doc = ie.Document;
+            doc.getElementById("username").innerText = "admin";
+            doc.getElementById("password").innerText = "wo123456";
+            System.Threading.Thread.Sleep(1000);
+            doc.getElementById("ulgin").click();
+        }
+
+        private bool ie_read = false;
+        private void ie_DocomentComplete(object pDisp, ref object URL) //加载完成事件
+        {
+            ie_read = true;
+        }
+
+        private void compWait() //等待，直到加载完成
+        {
+            while (ie_read != true)
+            {
+                Application.DoEvents();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SHDocVw.InternetExplorer ie = new SHDocVw.InternetExplorer();
+            ie.DocumentComplete += ie_DocomentComplete;
+            ie.Navigate("http://192.168.32.121/");
+            ie.Visible = true;
+            compWait();
+            mshtml.HTMLDocument doc = ie.Document;
+            mshtml.IHTMLElementCollection userids = doc.getElementsByName("username");
+            foreach (mshtml.IHTMLElement userid in userids)
+            {
+                userid.innerText = "admin";
+            }
+            mshtml.IHTMLElementCollection passwords = doc.getElementsByName("password");
+            foreach (mshtml.IHTMLElement password in passwords)
+            {
+                password.innerText = "wo123456";
+            }
+            mshtml.IHTMLElementCollection syaincds = doc.getElementsByName("syaincd");
+            foreach (mshtml.IHTMLElement syaincd in syaincds)
+            {
+                syaincd.innerText = "0";
+            }
+            mshtml.IHTMLElementCollection logins = doc.getElementsByName("ulgin");
+            foreach (mshtml.IHTMLElement login in logins)
+            {
+                login.click();
+            }
+
+            System.Threading.Thread.Sleep(3000);
+            mshtml.IHTMLElementCollection tencds = doc.getElementsByName("ulgin");
+            foreach (mshtml.IHTMLElement tencd in tencds)
+            {
+                tencd.click();
+            }
+        }
+    }
+}
